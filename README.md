171ef5a0f1762f435bac0f46bcaa0cdf4d51e598
Nathaniel J Cochran <nathanjcochran@gmail.com>
2020-03-29 17:44:23 -0400

    Improve usage information

--- README.md
+++ README.md
@@ -5,16 +5,38 @@ Tool for upgrading a go dependency's major version.
 ## Usage
 
 ```
-Usage: upgrade [-f modfile path] [-v] module [version]:
+Usage: upgrade [-d directory] [-v] [module [version]]
 
-Upgrades the named module dependency to the specified version,
-or, if no version is given, to the highest major version available.
+Upgrades the named module dependency to the specified version, or, if no
+version is given, to the highest major/minor/patch version available. Updates
+the dependency's require directive in the go.mod file, as well as the relevant
+import paths in all Go files in the current module (the current package and
+all subpackages).
 
-The module should be given as a fully qualified module path
-(including the major version component, if applicable).
-For example: github.com/nathanjcochran/gomod.
+The module should be given as a fully qualified module path, as it is written
+in the go.mod file (including the major version component, if applicable).
 
-  -f string
-    	go.mod file path (default "./go.mod")
+For example: github.com/nathanjcochran/upgrade
+
+The version must be a valid semver module version. It can be provided with any
+level of major/minor/patch specificity - e.g. 'v2', 'v2.3', 'v.2.3.4'. The
+tool will always attempt to upgrade to the highest available matching version.
+
+If no module is given, or if the special target "all" is given, the tool
+will attempt to upgrade all direct dependencies in the go.mod file to the
+highest major version available.
+
+Note that updating major versions of dependencies is likely to introduce
+compilation and/or runtime errors due to backwards-incompatibile changes. You
+should be prepared to address errors after the upgrade process completes.
+
+By default, the tool assumes the module to update is rooted in the current
+directory. The [-d directory] flag can be provided to override that behavior.
+
+The [-v] flag turns on verbose output.
+
+Options:
+  -d string
+    	Module directory path (default ".")
   -v	verbose output
 ```
--- main.go
+++ main.go
@@ -21,15 +21,37 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-const usage = `Usage: %s [-d module directory] [-v] module [version]
+const usage = `Usage: %s [-d directory] [-v] [module [version]]
 
-Upgrades the named module dependency to the specified version,
-or, if no version is given, to the highest major version available.
+Upgrades the named module dependency to the specified version, or, if no
+version is given, to the highest major/minor/patch version available. Updates
+the dependency's require directive in the go.mod file, as well as the relevant
+import paths in all Go files in the current module (the current package and
+all subpackages).
 
-The module should be given as a fully qualified module path
-(including the major version component, if applicable).
-For example: github.com/nathanjcochran/gomod
+The module should be given as a fully qualified module path, as it is written
+in the go.mod file (including the major version component, if applicable).
 
+For example: github.com/nathanjcochran/upgrade
+
+The version must be a valid semver module version. It can be provided with any
+level of major/minor/patch specificity - e.g. 'v2', 'v2.3', 'v.2.3.4'. The
+tool will always attempt to upgrade to the highest available matching version.
+
+If no module is given, or if the special target "all" is given, the tool
+will attempt to upgrade all direct dependencies in the go.mod file to the
+highest major version available.
+
+Note that updating major versions of dependencies is likely to introduce
+compilation and/or runtime errors due to backwards-incompatibile changes. You
+should be prepared to address errors after the upgrade process completes.
+
+By default, the tool assumes the module to update is rooted in the current
+directory. The [-d directory] flag can be provided to override that behavior.
+
+The [-v] flag turns on verbose output.
+
+Options:
 `
 
 var (
@@ -49,10 +71,7 @@ func main() {
 	path := flag.Arg(0)
 	version := flag.Arg(1)
 	switch path {
-	case "":
-		flag.Usage()
-		os.Exit(2)
-	case "all":
+	case "", "all":
 		upgradeAll(file)
 	default:
 		upgradeOne(file, path, version)
