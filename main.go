602a7b1511081d861d5d96dc3598a974ab76fe94
Nathaniel J Cochran <nathanjcochran@gmail.com>
2020-03-31 22:19:50 -0400

    Rewrite usage, given new functionality

--- main.go
+++ main.go
@@ -21,35 +21,47 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-const usage = `Usage: %s [-d directory] [-v] [module [version]]
+const usage = `Usage: %s [-d dir] [-v] [module] [version]
 
-Upgrades the named module dependency to the specified version, or, if no
-version is given, to the highest major/minor/patch version available. Updates
-the dependency's require directive in the go.mod file, as well as the relevant
-import paths in all Go files in the current module (the current package and
-all subpackages).
+Upgrades the major version of a module - or the major version of one of its
+dependencies - by editing the module's go.mod file and the corresponding import
+paths in its Go files.
 
-The module should be given as a fully qualified module path, as it is written
-in the go.mod file (including the major version component, if applicable).
+If no arguments are given, upgrades the major version of the module rooted in
+the current directory. Increments the major version component of its path in
+the go.mod file (adding the version component if necessary). The same behavior
+is triggered by supplying the module's own path for the [module] argument.
+However, in that form, a target [version] can also be given, making it possible
+to jump several major versions at once (or to downgrade versions).
 
-For example: github.com/nathanjcochran/upgrade
+If the module path of a dependency is given, upgrades the dependency to the
+specified version, or, if no version is given, to the highest major version
+available. Updates the dependency's require directive in the go.mod file, as
+well as the relevant import paths in all Go files in the current module (the
+current package and all subpackages).
 
-The version must be a valid semver module version. It can be provided with any
-level of major/minor/patch specificity - e.g. 'v2', 'v2.3', 'v.2.3.4'. The
-tool will always attempt to upgrade to the highest available matching version.
+If the special target "all" is given, the tool will attempt to upgrade all
+direct dependencies in the go.mod file to the highest major version available.
 
-If no module is given, or if the special target "all" is given, the tool
-will attempt to upgrade all direct dependencies in the go.mod file to the
-highest major version available.
+This tool does not add version tags in version control systems.
 
-Note that updating major versions of dependencies is likely to introduce
-compilation and/or runtime errors due to backwards-incompatibile changes. You
-should be prepared to address errors after the upgrade process completes.
+If given, [module] must be a fully qualified module path, as written in the
+go.mod file. It must include the major version component, if applicable. For
+example: "github.com/nathanjcochran/upgrade/v2".
 
-By default, the tool assumes the module to update is rooted in the current
+The [version], if given, must be a valid semver module version. It can be
+provided with any level of major/minor/patch specificity - e.g. 'v2', 'v2.3',
+'v.2.3.4'. When upgrading a dependency, the tool will attempt to upgrade to the
+highest available matching version, unless the target major version of the
+dependency is already required, in which case it will maintain the existing
+minor/patch version. When upgrading the current module, only the major
+component of the provided version is taken into account.
+
+By default, the tool assumes the module being updated is rooted in the current
 directory. The [-d directory] flag can be provided to override that behavior.
 
 The [-v] flag turns on verbose output.
+
 
 Options:
 `
