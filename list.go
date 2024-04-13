88b7657273059725edcdd516cbb0c53c6635f1be
Nathaniel J Cochran <nathan@popsql.com>
2024-04-13 14:06:56 -0400

    Update 'go help list' output structs

--- list.go
+++ list.go
@@ -23,18 +23,23 @@ func list(ctx context.Context) error {
 
 // From "go help list" output
 type Module struct {
-	Path      string       // module path
-	Version   string       // module version
-	Versions  []string     // available module versions (with -versions)
-	Replace   *Module      // replaced by this module
-	Time      *time.Time   // time version was created
-	Update    *Module      // available update, if any (with -u)
-	Main      bool         // is this the main module?
-	Indirect  bool         // is this module only an indirect dependency of main module?
-	Dir       string       // directory holding files for this module, if any
-	GoMod     string       // path to go.mod file for this module, if any
-	GoVersion string       // go version used in module
-	Error     *ModuleError // error loading module
+	Path       string       // module path
+	Query      string       // version query corresponding to this version
+	Version    string       // module version
+	Versions   []string     // available module versions
+	Replace    *Module      // replaced by this module
+	Time       *time.Time   // time version was created
+	Update     *Module      // available update (with -u)
+	Main       bool         // is this the main module?
+	Indirect   bool         // module is only indirectly needed by main module
+	Dir        string       // directory holding local copy of files, if any
+	GoMod      string       // path to go.mod file describing module, if any
+	GoVersion  string       // go version used in module
+	Retracted  []string     // retraction information, if any (with -retracted or -u)
+	Deprecated string       // deprecation message, if any (with -u)
+	Error      *ModuleError // error loading module
+	Origin     any          // provenance of module
+	Reuse      bool         // reuse of old module info is safe
 }
 
 type ModuleError struct {
